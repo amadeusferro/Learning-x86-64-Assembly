c127b36b7b614e301555d5207b6ed0d5f8e7ddb9
Amadeus Ferro <amadeus.ferro55@gmail.com>
2025-07-29 13:04:22 -0300

    Variadic Parameter III

--- Variadic Parameters/main.asm
+++ Variadic Parameters/main.asm
@@ -1,67 +1,154 @@
 section .data
-    fmt db "Numbers:  %d, %x, %f", 10, 0
+    fmt db "Numbers: %d, %x", 10, 0
     num_int dd 42
-    num_float dd 3.1415
+    int_buffer times 12 db 0
 
 section .text
     global _start
 
 _start:
-    push dword [num_float]
-    push 0xDEADBEEF
-    push dword [num_in]
+    push dword 0xDEADBEEF
+    push dword [num_int]
     push fmt
-    call my_prinf
-    add esp, 16
+    call my_printf
+    add esp, 12
 
+    ; exit(0)
     mov eax, 1
-    mov ebx, 0
+    xor ebx, ebx
     int 0x80
 
-my_prinf:
+; ===================== printf simplificado ======================
+my_printf:
     push ebp
     mov ebp, esp
+    push ebx            
+    push esi              
 
-    ; ebp+8  = format
-    ; ebp+12 = first variadic param
-    ; ebp+16 = second variadic param
-    ; ...
-
-    mov esi, [ebp+8]
-    mov edi, 12
+    mov esi, [ebp+8]      
+    lea ebx, [ebp+12]      
 
 .process_fmt:
-    lodsb
-    cmp al, 0
-    je .end
+    lodsb                 
+    test al, al
+    jz .end
 
     cmp al, '%'
-    jne .print_chat
+    jne .print_char
 
-    lodsb
+    lodsb                  
     cmp al, 'd'
     je .print_int
     cmp al, 'x'
     je .print_hex
-    ; ... others
+    jmp .process_fmt    
 
 .print_int:
-    mov eax, [ebp+edi]
-    add edi, 4
-    ; print int
+    mov eax, [ebx]
+    add ebx, 4
+    push eax
+    call print_int
+    add esp, 4
     jmp .process_fmt
 
 .print_hex:
-    mov eax, [ebp+edi]
-    add edi, 4
-    ; print hex
+    mov eax, [ebx]
+    add ebx, 4
+    push eax
+    call print_hex
+    add esp, 4
     jmp .process_fmt
 
 .print_char:
-    ; normal print
+    push eax              
+    mov eax, 4          
+    mov ecx, esp         
+    mov edx, 1          
+    push ebx              
+    mov ebx, 1       
+    int 0x80
+    pop ebx              
+    pop eax            
     jmp .process_fmt
 
 .end:
+    pop esi               
+    pop ebx              
     mov esp, ebp
+    pop ebp
+    ret
+
+print_int:
+    push ebp
+    mov ebp, esp
+    push ebx                
+    push edi                
+
+    mov eax, [ebp+8]       
+    mov edi, int_buffer + 11
+    mov byte [edi], 0      
+    mov ecx, 10           
+
+    test eax, eax           
+    jnz .convert_loop
+    mov byte [edi-1], '0'  
+    dec edi
+    jmp .print
+
+.convert_loop:
+    xor edx, edx
+    div ecx
+    add dl, '0'
+    dec edi
+    mov [edi], dl
+    test eax, eax
+    jnz .convert_loop
+
+.print:
+    mov eax, 4           
+    mov ebx, 1             
+    mov ecx, edi         
+    mov edx, int_buffer + 11
+    sub edx, edi          
+    int 0x80
+
+    pop edi              
+    pop ebx               
+    pop ebp
+    ret
+
+print_hex:
+    push ebp
+    mov ebp, esp
+    push ebx               
+    push edi              
+
+    mov eax, [ebp+8]       
+    mov edi, int_buffer + 8 
+    mov ecx, 8            
+
+.hex_loop:
+    mov edx, eax
+    and edx, 0xF          
+    cmp dl, 9
+    jbe .digit
+    add dl, 'A' - 10      
+    jmp .store
+.digit:
+    add dl, '0'           
+.store:
+    dec edi
+    mov [edi], dl
+    shr eax, 4            
+    loop .hex_loop
+
+    mov eax, 4            
+    mov ebx, 1           
+    mov ecx, edi       
+    mov edx, 8            
+    int 0x80
+
+    pop edi             
+    pop ebx                
     pop ebp
     ret
